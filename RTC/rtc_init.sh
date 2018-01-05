1f4a418d743ed863f31f8f3ea6cc8173aea52409
Kiseev Nikolay <kiseev.nikolay@gmail.com>
2018-01-06 01:46:13 +0800

    Fix rtc_init.sh

--- RTC/rtc_init.sh
+++ RTC/rtc_init.sh
@@ -2,11 +2,12 @@
 
 cnt=`ls /dev/rtc1 | wc -l`
 
-while true; do
-  sleep 10
-  cnt1=`ls /dev/rtc1 | wc -l`
-  [ $cnt1 -ne $cnt ] && exit
-  echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-2/new_device &>> /tmp/rtc.err.log
+while [ $cnt -eq 0 ]
+do
+  echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-2/new_device
+  sleep 5
+  cnt=`ls /dev/rtc1 | wc -l`
 done
 
-hwclock -D -s -f /dev/rtc1 &>> /tmp/hwclock.err.log
+sleep 5
+hwclock -s -f /dev/rtc1
